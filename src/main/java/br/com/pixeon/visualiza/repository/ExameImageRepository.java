a4c4c284dd75daaddce224edfbace600f8f5b868
Ricardo <ricardwrodrigues@gmail.com>
2016-06-29 10:11:56 -0300

    Correção de import

--- src/main/java/br/com/pixeon/visualiza/repository/ExameImageRepository.java
+++ src/main/java/br/com/pixeon/visualiza/repository/ExameImageRepository.java
@@ -1,6 +1,5 @@
 package br.com.pixeon.visualiza.repository;
 
-import br.com.pixeon.visualiza.domain.Exame;
 import br.com.pixeon.visualiza.domain.ExameImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
